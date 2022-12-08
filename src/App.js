4a351febc6d4c07b691efd5aee933dcf0fb79dff
Robert Davis <robertcd@hawaii.edu>
2022-12-07 15:51:37 -1000

    lab 6 process

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import './App.css';
-import Main from './components/Main';
-import About from './components/About';
-import Search from './components/Search';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import React from 'react';
+import './App.css';
+// import Main from './components/Main';
+// import About from './components/About';
+import Search from './components/Search';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <header>
-            <h1>Welcome to City Explorer</h1>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/About">About</Link>
-                </li>
-                <li>
-                  <Link to="/Search">Search</Link>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="city" element={<City />} />
+      <Search></Search>
+      // <BrowserRouter>
+      //   <div className="App">
+      //     <header>
+      //       <h1>Welcome to City Explorer</h1>
+      //       <nav>
+      //         <ul>
+      //           <li>
+      //             <Link to="/">Home</Link>
+      //           </li>
+      //           <li>
+      //             <Link to="/About">About</Link>
+      //           </li>
+      //           <li>
+      //             <Link to="/Search">Search</Link>
+      //           </li>
+      //         </ul>
+      //       </nav>
+      //     </header>
+      //     <Routes>
+      //       {/* <Route path="/" element={<Main />} /> */}
+      //       <Route path="Search" element={<Search />} />
 
-          </Routes>
-        </div>
-      </BrowserRouter>
+      //     </Routes>
+      //   </div>
+      // </BrowserRouter>
     );
   }
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import map from '../images/map.png';
-import restaurantData from '../data/restaurants.json';
-import locationData from '../data/location.json';
+// import map from '../images/map.png';
+// import restaurantData from '../data/restaurants.json';
+// import locationData from '../data/location.json';
 import axios from 'axios';
-import Alert from 'react-bootstrap/Alert';
-import Button from 'react-bootstrap/Button';
+// import Alert from 'react-bootstrap/Alert';
+// import Button from 'react-bootstrap/Button';
 const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 class Search extends React.Component {
@@ -12,34 +12,39 @@ class Search extends React.Component {
     super();
     this.state = {
       locationSearch: '',
-      restaurantData: restaurantData,
-      locationData: locationData,
+      // restaurantData: restaurantData,
+      // locationData: locationData,
       error: null,
     }
   }
-
+saveCity = (e) => {
+  console.log(e.target.value)
+  this.setState ({
+    locationSearch: e.target.value
+  })
+}
   // this should query location IQ for geolocation data
   handleLocationSearch = async (e) => {
     e.preventDefault();
-    let request = {
-      method: 'GET',
-      url: `https://us1.locationiq.com/v1/search?key=${ACCESS_KEY}&q=${e.target.search.value}&format=json`
-    }
+  //  // let request = {
+  //     method: 'GET',
+  //     url: `https://us1.locationiq.com/v1/search?key=${ACCESS_KEY}&q=${e.target.search.value}&format=json`
+  //   }
 
-    // make our location IQ request;
-    try {
-      let response = await axios(request);
+  //   // make our location IQ request;
+  //   try {
+  //     let response = await axios(request);
 
-      // we never get to this line of code if we get an error;
-      this.setState({
-        locationSearch: e.target.search.value,
-        locationData: response.data[0],
-      }, () => this.handlePokemonSearch('pikachu'));
-      // really handy for examining and capturing errors.
-    } catch (err) {
-      console.log('Error occurred while requesting');
-      this.setState({ error: err.response.data });
-    }
+  //     // we never get to this line of code if we get an error;
+  //     this.setState({
+  //       locationSearch: e.target.search.value,
+  //       locationData: response.data[0],
+  //     }, () => this.handlePokemonSearch('pikachu'));
+  //     // really handy for examining and capturing errors.
+  //   } catch (err) {
+  //     console.log('Error occurred while requesting');
+  //     this.setState({ error: err.response.data });
+  //   }
   }
 
   handlePokemonSearch = async (pokemonName) => {
@@ -65,43 +70,43 @@ class Search extends React.Component {
   } 
 
   render() {
-    console.log(this.state);
-    return (
-      <div id="city-search">
-        <form onSubmit={this.handleLocationSearch}>
-          <label>Search for a location</label>
-          <input type="text" name="search" placeholder="Enter City here"/>
-          <button type="submit">Explore!</button>
-        </form>
-        {this.state.error
-          ? <Alert>
-              {JSON.stringify(this.state.error)}
-              <Button onClick={this.handleError}>Thanks go away</Button>
-            </Alert>
-          : null
-        }
-        {this.state.locationData 
-          ? <p>{this.state.locationData.display_name}</p>
-          : <p>Please search for a city!</p>
-        }
-        {this.state.pokemonData
-          ? <div>
-              <p>Name: {this.state.pokemonData.name}</p>
-              <p>type: {this.state.pokemonData.type}</p>
-            </div>
-          : null
-        }
-        {this.state.locationSearch && this.state.locationData
-          ? <div id="map"><img src={map} alt="location map"/></div>
-          : null
-        }
-        {this.state.locationSearch && this.state.restaurantData
-          ? <ul>{this.state.restaurantData.map(place => <li>{place.restaurant}</li>)}</ul>
-          : null
-        }
-      </div>
-    )
-  }
+     return (
+      <h2>Here</h2>
+  //     <div id="city-search">
+  //       <form onSubmit={this.handleLocationSearch}>
+  //         <label>Search for a location</label>
+  //         <input onChange={this.saveCity} type="text" name="search" placeholder="Enter City here"/>
+  //         <button type="submit">Explore!</button>
+  //       </form>
+  //       {this.state.error
+  //         ? <Alert>
+  //             {JSON.stringify(this.state.error)}
+  //             <Button onClick={this.handleError}>Thanks go away</Button>
+  //           </Alert>
+  //         : null
+  //       }
+  //       {this.state.locationData 
+  //         ? <p>{this.state.locationData.display_name}</p>
+  //         : <p>Please search for a city!</p>
+  //       }
+  //       {this.state.pokemonData
+  //         ? <div>
+  //             <p>Name: {this.state.pokemonData.name}</p>
+  //             <p>type: {this.state.pokemonData.type}</p>
+  //           </div>
+  //         : null
+  //       }
+  //       {this.state.locationSearch && this.state.locationData
+  //         ? <div id="map"><img src={map} alt="location map"/></div>
+  //         : null
+  //       }
+  //       {this.state.locationSearch && this.state.restaurantData
+  //         ? <ul>{this.state.restaurantData.map(place => <li>{place.restaurant}</li>)}</ul>
+  //         : null
+  //       }
+  //     </div>
+     )
+   }
 }
 
 export default Search;
