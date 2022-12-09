fdc38a9f9912e06f9c281ec8d254ecb6215d4270
Robert Davis <robertcd@hawaii.edu>
2022-12-08 15:20:27 -1000

    adds my city explorer location

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,16 +4,22 @@ class Main extends React.Component {
   constructor() {
     super();
     this.state = {
+      show: true,
     }
   }
 
+  toggleShow = () => {
+    this.setState({ show: !this.state.show });
+  }
 
   render() {
     return (
-   <></>
+      <div>
+        <p>Welcome to the Main Component! <span onClick={this.toggleShow}>Click me to show something below</span></p>
+        {this.state.show ? <p>This should render conditionally</p> : null}
+      </div>
     )
   }
 }
 
 export default Main;
-
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 // import restaurantData from '../data/restaurants.json';
 // import locationData from '../data/location.json';
 import axios from 'axios';
-// import Alert from 'react-bootstrap/Alert';
-// import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY;
 
 class Search extends React.Component {
@@ -13,54 +13,38 @@ class Search extends React.Component {
     this.state = {
       locationSearch: '',
       // restaurantData: restaurantData,
-      // locationData: locationData,
+      // locationData: {},
+      weatherData: [],
       error: null,
     }
   }
+
 saveCity = (e) => {
   console.log(e.target.value)
   this.setState ({
     locationSearch: e.target.value
   })
 }
-  // this should query location IQ for geolocation data
-  handleLocationSearch = async (e) => {
-    e.preventDefault();
-  //  // let request = {
-  //     method: 'GET',
-  //     url: `https://us1.locationiq.com/v1/search?key=${ACCESS_KEY}&q=${e.target.search.value}&format=json`
-  //   }
-
-  //   // make our location IQ request;
-  //   try {
-  //     let response = await axios(request);
-
-  //     // we never get to this line of code if we get an error;
-  //     this.setState({
-  //       locationSearch: e.target.search.value,
-  //       locationData: response.data[0],
-  //     }, () => this.handlePokemonSearch('pikachu'));
-  //     // really handy for examining and capturing errors.
-  //   } catch (err) {
-  //     console.log('Error occurred while requesting');
-  //     this.setState({ error: err.response.data });
-  //   }
+//query location IQ for geoloaciton data
+handleLocationSearch =async (e) => {
+  e.preventDefault();
+  let request = {
+    method: "GET",
+    url: `https://us1.locationiq.com/v1/search?key=${ACCESS_KEY}&q=${e.target.search.value}&format=json`
   }
 
-  handlePokemonSearch = async (pokemonName) => {
-    let request = {
-      method: 'GET',
-      url: `http://localhost:3001/pokemon?name=${pokemonName}`
-    }
-    try {
-      let response = await axios(request);
+  try {
+    let response = await axios (request);
+    console.log('this is our response', response.data[0]);
+    
+    this.setState({
+      locationSearch: e.target.value,
+      locationData: response.data[0],
 
-      this.setState({
-        pokemonData: response.data,
-      });
+    })
     } catch (err) {
-      console.log('Error occurred while requesting');
-      this.setState({ error: err.response.data });
+      console.log(err);
+      this.handleError(err.response.data);
     }
 
   };
@@ -69,44 +53,29 @@ saveCity = (e) => {
     this.setState({ error: null });
   } 
 
-  render() {
-     return (
-      <h2>Here</h2>
-  //     <div id="city-search">
-  //       <form onSubmit={this.handleLocationSearch}>
-  //         <label>Search for a location</label>
-  //         <input onChange={this.saveCity} type="text" name="search" placeholder="Enter City here"/>
-  //         <button type="submit">Explore!</button>
-  //       </form>
-  //       {this.state.error
-  //         ? <Alert>
-  //             {JSON.stringify(this.state.error)}
-  //             <Button onClick={this.handleError}>Thanks go away</Button>
-  //           </Alert>
-  //         : null
-  //       }
-  //       {this.state.locationData 
-  //         ? <p>{this.state.locationData.display_name}</p>
-  //         : <p>Please search for a city!</p>
-  //       }
-  //       {this.state.pokemonData
-  //         ? <div>
-  //             <p>Name: {this.state.pokemonData.name}</p>
-  //             <p>type: {this.state.pokemonData.type}</p>
-  //           </div>
-  //         : null
-  //       }
-  //       {this.state.locationSearch && this.state.locationData
-  //         ? <div id="map"><img src={map} alt="location map"/></div>
-  //         : null
-  //       }
-  //       {this.state.locationSearch && this.state.restaurantData
-  //         ? <ul>{this.state.restaurantData.map(place => <li>{place.restaurant}</li>)}</ul>
-  //         : null
-  //       }
-  //     </div>
-     )
-   }
-}
+    render() {
+      console.log(this.state);
+      return (
+        <div id="city-search">
+          <form onSubmit={this.handleLocationSearch}>
+            <label>Search for a location</label>
+            <input type="text" name="search" placeholder="Enter City here" />
+            <button type="submit">Explore!</button>
+          </form>
+          {this.state.error
+            ? <Alert>
+              {JSON.stringify(this.state.error)}
+              <Button onClick={() => this.handleError()}>Thanks go away</Button>
+            </Alert>
+            : null
+          }
+          {this.state.locationData
+            ? <p>{this.state.locationData.display_name}</p>
+            : <p>Please search for a city!</p>
+          }
+          </div>
+      )
+    }
+  }
 
 export default Search;
